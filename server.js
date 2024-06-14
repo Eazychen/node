419d671bd097caee2a78b8564e6cf664bf3b634c
eazy <eazy_chen@icloud.com>
2024-06-15 04:12:36 +0800

    Update CORS settings

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ require("dotenv").config();
 const app = express();
 
 const corsOptions = {
-	origin: process.env.CORS_ORIGIN || "https://limingelectronics.netlify.app", // 允許來自此來源的請求
+	origin:
+		process.env.CORS_ORIGIN || "https://limingelectronics.netlify.app/services", // 允許來自此來源的請求
 	methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
 	allowedHeaders: ["Content-Type", "Authorization"],
 	optionsSuccessStatus: 200,
