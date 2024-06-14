69a91223ef21b603d9ed496955be5aa79b931fa9
eazy <eazy_chen@icloud.com>
2024-06-15 03:18:00 +0800

    cros update

--- server.js
+++ server.js
@@ -8,8 +8,7 @@ require("dotenv").config();
 const app = express();
 
 const corsOptions = {
-	origin: process.env.CORS_ORIGIN || "http://localhost:3000", // 允許來自此來源的請求
-	methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+	origin: process.env.CORS_ORIGIN || "https://limingelectronics.netlify.app", // 允許來自此來源的請求
 	optionsSuccessStatus: 200,
 };
 
@@ -19,6 +18,7 @@ app.use(express.json());
 app.use("/api", lineRouter);
 app.use("/api", servicesRouter);
 
+app.options("/api/submitForm", cors(corsOptions));
 app.listen(5001, () => {
 	console.log("server is running 5001");
 });
