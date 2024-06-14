78e8fb4969ccdc8b31e93b52b3ab8626a0b86579
eazy <eazy_chen@icloud.com>
2024-06-15 01:56:14 +0800

    connection change to heroku pg

--- Routers/services.js
+++ Routers/services.js
@@ -6,11 +6,10 @@ require("dotenv").config();
 const router = express.Router();
 
 const config = {
-	user: process.env.PG_USER,
-	host: process.env.PG_HOST,
-	database: process.env.PG_DATABASE,
-	password: process.env.PG_PASSWORD || "",
-	port: process.env.PG_PORT,
+	connectionString: process.env.DATABASE_URL,
+	ssl: {
+		rejectUnauthorized: false,
+	},
 };
 
 const client = new pg.Client(config);
