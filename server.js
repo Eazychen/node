e16f8fb465e131d6a71f86a8654b4d9ac3008edc
eazy <eazy_chen@icloud.com>
2024-06-15 03:47:12 +0800

    add cros  method

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const app = express();
 
 const corsOptions = {
 	origin: process.env.CORS_ORIGIN || "https://limingelectronics.netlify.app", // 允許來自此來源的請求
+	methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
 	optionsSuccessStatus: 200,
 };
 
@@ -19,6 +20,6 @@ app.options("*", cors(corsOptions));
 app.use("/api", lineRouter);
 app.use("/api", servicesRouter);
 
-app.listen(5001, () => {
+app.listen(process.env.PORT, 5001, () => {
 	console.log("server is running 5001");
 });
