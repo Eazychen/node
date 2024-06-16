23e9dbc4eb73eb3ff265425d89bdbb5d5dee499b
eazy <eazy_chen@icloud.com>
2024-06-17 02:18:39 +0800

    config testing

--- Routers/line.js
+++ Routers/line.js
@@ -9,12 +9,6 @@ const config = {
 	channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
 };
 
-if (!config.channelSecret || !config.channelAccessToken) {
-	console.log(
-		"LINE bot configuration error: missing channelSecret or channelAccessToken"
-	);
-}
-
 eventHandler = async (event) => {
 	if (event.type !== "message" || event.message.type !== "text") {
 		return Promise.resolve(null);
@@ -22,15 +16,26 @@ eventHandler = async (event) => {
 	console.log(event.message.text);
 };
 
-router.post("/webhook", (req, res) => {
-	console.log(config);
-	Promise.all(req.body.events.map(eventHandler))
-		.then((result) => {
-			return res.status(200).json(result);
-		})
-		.catch((err) => {
-			console.error(err.stack);
-			return res.status(500).send("Error handling message");
-		});
-});
+router.post(
+	"/webhook",
+	(req, res, next) => {
+		console.log("Config before middleware:", config);
+		next();
+	},
+	line.middleware(config),
+	(req, res, next) => {
+		console.log("Middleware has processed the request", config);
+		next();
+	},
+	(req, res) => {
+		Promise.all(req.body.events.map(eventHandler))
+			.then((result) => {
+				return res.status(200).json(result);
+			})
+			.catch((err) => {
+				console.error(err.stack);
+				return res.status(500).send("Error handling message");
+			});
+	}
+);
 module.exports = router;
